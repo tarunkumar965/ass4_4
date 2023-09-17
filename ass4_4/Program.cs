487fac1f5fd5c19d3c5c0ccfaa25a1f5aea1cacf
tarun <tarun@LAPTOP-384VCLUN>
2023-09-17 18:51:52 +0530

    Add project files.

---
+++ ass4_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass4_4
+{
+    public class Temperature
+    {
+        private double celsius;
+
+      
+        public double Celsius
+        {
+            get { return celsius; }
+            set { celsius = value; }
+        }
+
+        
+        public double Fahrenheit
+        {
+            get
+            {
+              
+                return (celsius * 9 / 5) + 32;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp = new Temperature();
+            temp.Celsius = 25.0; 
+
+         
+            Console.WriteLine($"Temperature in Celsius: {temp.Celsius}°C");
+            Console.WriteLine($"Temperature in Fahrenheit: {temp.Fahrenheit}°F");
+        }
+    }
+}
